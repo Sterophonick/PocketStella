fd36ccf596328584cb751dd82b6b55884c61bdb3
Sterophonick <34801996+Sterophonick@users.noreply.github.com>
2019-11-17 08:54:18 -0700

    Create macros for the various 6502 instructions

--- inc/m6502.h
+++ inc/m6502.h
@@ -1,1 +1,8 @@
+extern u8 a26AC;
+extern u8 a26X;
+extern u8 a26Y;
+extern u16 a26PC;
+extern u8 a26SR;
+extern u8 a26SP;
+
 extern void a26Execute();
--- src/m6502.c
+++ src/m6502.c
@@ -1,4 +1,6 @@
 //MOS Technology 6502 simulation
+//Credits to https://www.masswerk.at/6502/6502_instruction_set.html
+
 #include <libheart.h>
 #include "lang.h"
 #include "emu.h"
@@ -6,6 +8,74 @@
 #include "vram.h"
 #include "m6502.h"
 #include "vmachine.h"
+u8 a26AC = 0;
+u8 a26X = 0;
+u8 a26Y = 0;
+u16 a26PC = 0;
+u8 a26SR = 0;
+u8 a26SP = 0;
+
+#define ADC
+#define AND
+#define ASL
+#define BCC
+#define BCS
+#define BEQ
+#define BIT
+#define BMI
+#define BNE
+#define BPL
+#define BRK
+#define BVC
+#define BVS
+#define CLC
+#define CLD
+#define CLI
+#define CLV
+#define CMP
+#define CPX
+#define CPY
+#define DEC
+#define DEX
+#define DEY
+#define EOR
+#define INC
+#define INX
+#define INY
+#define JMP
+#define JSR
+#define LDA
+#define LDX
+#define LDY
+#define LSR
+#define NOP
+#define ORA
+#define PHA
+#define PHP
+#define PLA
+#define PLP
+#define ROL
+#define ROR
+#define RTI
+#define RTS
+#define SBC
+#define SEC
+#define SED
+#define SEI
+#define STA
+#define STX
+#define STY
+#define TAX
+#define TAY
+#define TSX
+#define TXA
+#define TSX
+#define TYA
+
+void _und()
+{
+
+}
 
 void a26Execute()
 {
